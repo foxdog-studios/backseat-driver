fd85c474e0c4b519b52195e3c8ebca9538d912d2
Peter Sutton <foxxy@foxdogstudios.com>
2014-03-14 09:54:19 +0000

    Hat can be controlled via the web

--- python/hat.py
+++ python/hat.py
@@ -13,6 +13,8 @@ import time
 
 import ddp
 
+from pyfirmata import Arduino
+
 LOG_LEVELS = (
     logging.CRITICAL,
     logging.ERROR,
@@ -28,14 +30,16 @@ LOG_NAME_TO_LEVEL = OrderedDict((name, level)
 
 
 class HatController(object):
-    def __init__(self, url):
+    def __init__(self, url, board):
         self._conn = ddp.DdpConnection(
             url,
             received_message_callback=self._recieved_message,
         )
+
+        self._board = board
         self._id = None
-        self._is_ready = False
         self._is_activated = False
+        self._is_ready = False
 
     def _recieved_message(self, message):
         hat = u'hat'
@@ -44,6 +48,7 @@ class HatController(object):
 
         if isinstance(message, ddp.ReadyMessage):
             self._is_ready = True
+            self._set_hat_state(self._is_activated)
         elif isinstance(message, ddp.AddedMessage):
             fields = message.fields
             if fields.get(name) == hat:
@@ -62,9 +67,14 @@ class HatController(object):
         self._is_activated = is_activated
 
         if self._is_activated:
+            self._write(1)
             logger.debug('Hat activated')
         else:
+            self._write(0)
             logger.debug('Hat deactivated')
+
+    def _write(self, value):
+        self._board.digital[13].write(value)
 
     def connect(self):
         self._conn.connect()
@@ -85,6 +95,7 @@ def main(argv=None):
     parser.add_argument('-l', '--log-level', choices=LOG_NAME_TO_LEVEL.keys(),
                         default=LOG_LEVEL_TO_NAMES[logging.INFO])
     parser.add_argument('-s', '--server', default='127.0.0.1:3000')
+    parser.add_argument('device')
     args = parser.parse_args(args=argv[1:])
 
     logging.basicConfig(
@@ -95,17 +106,32 @@ def main(argv=None):
     logger = logging.getLogger(__name__)
 
     url = ddp.ServerUrl(args.server)
-    hat_controller = HatController(url)
+    board = None
+    hat_controller = None
+
     try:
+        logger.info('Connection to Arduino board...')
+        board = Arduino(args.device)
+        logger.info('Connecting to DDP server...')
+        hat_controller = HatController(url, board)
         hat_controller.connect()
         wait_for_user_to_exit()
     finally:
-        try:
-            hat_controller.disconnect()
-        except BaseException as e:
-            logger.exception(
-                'An error occured while disconnecting from the DDP server.'
-            )
+        if hat_controller is not None:
+            try:
+                hat_controller.disconnect()
+            except BaseException as e:
+                logger.exception(
+                    'An error occurred while disconnecting from the DDP '
+                    'server.'
+                )
+        if board is not None:
+            try:
+                board.exit()
+            except BaseException as e:
+                logger.exception(
+                    'An error occurred while exiting from the Arduino board.'
+                )
 
     return 0
 
